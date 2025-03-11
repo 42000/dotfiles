a27c137105411f26f3d308a321b7039d354a2dae
42000 <ptimorveux@gmail.com>
2025-03-11 19:02:05 +0100

    main xd

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ ln -s "$(pwd)/shellScripts" ~
 ln -s "$(pwd)/.mimeapps.list" ~/.config
 ln -s "$(pwd)/nvim" ~/.config
 ln -s "$(pwd)/foot" ~/.config
-# ln -s "$(pwd)/hypr" ~/.config
 ln -s "$(pwd)/dunst" ~/.config
 ln -s "$(pwd)/waybar" ~/.config
 ln -s "$(pwd)/swayimg" ~/.config
@@ -23,3 +22,15 @@ ln -s "$(pwd)/kakoune-lsp" ~/.config
 ln -s "$(pwd)/sway" ~/.config
 ln -s "$(pwd)/i3status" ~/.config
 # ln -s "$(pwd)/wezterm" ~/.config
+
+#   FONTS
+FONT_DIR=~/.local/share/fonts/
+mkdir -p $FONT_DIR
+ln -s "$(pwd)/fonts/"* $FONT_DIR
+
+#   LES TRUCS HYPER
+HYPRDIR=~/.config/hypr
+mkdir -p $HYPRDIR
+ln -s "$(pwd)/hypr/hyprlock.conf" $HYPRDIR
+# ln -s "$(pwd)/hypr/hyprland.conf" $HYPRDIR
+# ln -s "$(pwd)/hypr/hyprpaper.conf" $HYPRDIR
